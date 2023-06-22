82fd8876461dd63f5b79695305cba668e6ee03e8
Danilo Francisquete <marketing@letscode.com.br>
2023-06-22 13:22:57 -0300

    new

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { TaskList } from './../../model/task-list';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+ 
+  public tasKList:Array<TaskList>=[
+
+  ];
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+  public deleteItemTaskList(event:number){
+    this.tasKList.splice(event,1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente Deletar tudo?");
+   
+    if(confirm){
+      this.tasKList =[];
+    }
+  }
 
 }
